7bf4efb6e617b2555c0903602d98f721dc919f3a
Phoenix_13 <47122082+PhoenixGames-Phoenix@users.noreply.github.com>
2020-09-20 21:21:59 +0200

    Started config command

---
+++ src/commands/config.js
@@ -0,0 +1,17 @@
+var discord = require('discord.js');
+const {config} = require('../config.js');
+
+module.exports = {
+    name: 'config',
+    description: 'Sets or gets the config, Bot owner only',
+    async execute(message, args) {
+        if (message.author.tag != config.botowner.toString()) return await message.channel.send(config.messages.unauthorized);
+        if (!args[1]) return await message.channel.send(config.messages.notenoughargs);
+
+        if (args[0] == 'get') {
+            
+        } else if (args[0] == 'set') {
+
+        }
+    }
+}
