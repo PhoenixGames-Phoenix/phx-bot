126a699eb8ec263c5d6ddcac1c049a70e343e1ac
Phoenix_13 <47122082+PhoenixGames-Phoenix@users.noreply.github.com>
2020-09-20 19:57:55 +0200

    Added reload command

---
+++ src/commands/reload.js
@@ -0,0 +1,26 @@
+var discord = require('discord.js');
+const {config} = require('../config.js');
+
+module.exports = {
+    name: 'reload',
+    description: 'Reloads all commands, Bot Owner Only',
+    async execute(message, args) {
+        if (message.author.tag != config.botowner.toString()) return await message.channel.send(config.messages.unauthorized);
+        if (!args.length) return await message.channel.send(config.messages.notenoughargs);
+        const commandName = args[0].toLowerCase();
+        const command = message.client.commands.get(commandName) || message.client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+
+        if(!command) return await message.channel.send(config.messages.unknwoncommand);
+
+        delete require.cache[require.resolve(`./${command.name}.js`)];
+
+        try {
+            const newCommand = require(`./${command.name}.js`);
+            await message.client.commands.set(newCommand.name, newCommand);
+            await message.channel.send(`Successfuly reloaded command ${commandName}`);
+        } catch (error) {
+            console.error(error);
+            throw error;
+        }
+    }
+}
