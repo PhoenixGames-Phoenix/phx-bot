dcd74bdad55aca2049dc37548662baca46314df0
Phoenix_13 <47122082+PhoenixGames-Phoenix@users.noreply.github.com>
2020-10-21 00:43:49 +0200

    Added prettier!

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -1,8 +1,9 @@
-
 module.exports = {
-    name: 'ping',
-    description: 'Displays the current delay between the bot and the API',
-    async execute(message){
-        await message.channel.send(`Current Delay: ${message.client.ws.ping}ms :hourglass:`);
-    }
-}
+	name: "ping",
+	description: "Displays the current delay between the bot and the API",
+	async execute(message) {
+		await message.channel.send(
+			`Current Delay: ${message.client.ws.ping}ms :hourglass:`
+		);
+	},
+};
--- src/commands/reload.js
+++ src/commands/reload.js
@@ -1,26 +1,33 @@
-const config = require('../config.js').loadconfig();
+const config = require("../config.js").loadconfig();
 
 module.exports = {
-    name: 'reload',
-    description: 'Reloads all commands, Bot Owner Only',
-    usage: 'reload <command>',
-    async execute(message, args) {
-        if (message.author.tag != config.botowner.toString()) return await message.channel.send(config.messages.unauthorized);
-        if (!args.length) return await message.channel.send(config.messages.notenoughargs);
-        const commandName = args[0].toLowerCase();
-        const command = message.client.commands.get(commandName) || message.client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+	name: "reload",
+	description: "Reloads all commands, Bot Owner Only",
+	usage: "reload <command>",
+	async execute(message, args) {
+		if (message.author.tag != config.botowner.toString())
+			return await message.channel.send(config.messages.unauthorized);
+		if (!args.length)
+			return await message.channel.send(config.messages.notenoughargs);
+		const commandName = args[0].toLowerCase();
+		const command =
+			message.client.commands.get(commandName) ||
+			message.client.commands.find(
+				(cmd) => cmd.aliases && cmd.aliases.includes(commandName)
+			);
 
-        if(!command) return await message.channel.send(config.messages.unknwoncommand);
+		if (!command)
+			return await message.channel.send(config.messages.unknwoncommand);
 
-        delete require.cache[require.resolve(`./${command.name}.js`)];
+		delete require.cache[require.resolve(`./${command.name}.js`)];
 
-        try {
-            const newCommand = require(`./${command.name}.js`);
-            await message.client.commands.set(newCommand.name, newCommand);
-            await message.channel.send(`Successfuly reloaded command ${commandName}`);
-        } catch (error) {
-            console.error(error);
-            throw error;
-        }
-    }
-}
+		try {
+			const newCommand = require(`./${command.name}.js`);
+			await message.client.commands.set(newCommand.name, newCommand);
+			await message.channel.send(`Successfuly reloaded command ${commandName}`);
+		} catch (error) {
+			console.error(error);
+			throw error;
+		}
+	},
+};
